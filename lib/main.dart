8c81f9f57c9761699859e26de3ca638bb99d95c7
farzad seyyedzadeh <iamseyfarsey@gmail.com>
2024-01-11 22:23:06 +0330

    theme

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class IrangardshoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       theme: ThemeData(useMaterial3: true),
       home: const HomeScreen(),
     );
